f0d3fa1e3f338a70e67c992c37041c78b2b7622e
TerracoreDevelopment <server.managment@outlook.com>
2024-09-07 21:10:02 +0200

    added Mod items class

--- src/main/java/net/terracore/terracorecreate/TCoreCreateMod.java
+++ src/main/java/net/terracore/terracorecreate/TCoreCreateMod.java
@@ -1,6 +1,7 @@
 package net.terracore.terracorecreate;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -13,6 +14,7 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.terracore.terracorecreate.item.ModItems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -27,6 +29,8 @@ public class TCoreCreateMod
     public TCoreCreateMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
 
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
@@ -47,7 +51,10 @@ public class TCoreCreateMod
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.NATURAL_BLOCKS) {
+            event.accept(ModItems.MUTTERKORN);
+            //Siehe main.java.net.terracore.terracorecreate.item.ModItems für eine Erklärung, was Mutterkorn ist
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
--- src/main/java/net/terracore/terracorecreate/item/ModItems.java
+++ src/main/java/net/terracore/terracorecreate/item/ModItems.java
@@ -1,4 +1,22 @@
 package net.terracore.terracorecreate.item;
 
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.RegistryObject;
+import net.terracore.terracorecreate.TCoreCreateMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
 public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TCoreCreateMod.MOD_ID);
+
+    public static final RegistryObject<Item> MUTTERKORN = ITEMS.register("mutterkorn",
+            () -> new Item(new Item.Properties()));
+            //Mutterkorn, auch Wolfsklaue genannt, ist ein Pilz, der Roggen befällt und zur Herstellung von LSD benutzt wird
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
 }
