52a0a278deb026ce2a0aa5e9b2fbef4db47b07e8
TerracoreDevelopment <server.managment@outlook.com>
2024-09-26 20:06:30 +0200

    Made COCAINE_BLOCK FallingBlock

--- src/main/java/net/terracore/terracorecreate/block/ModBlocks.java
+++ src/main/java/net/terracore/terracorecreate/block/ModBlocks.java
@@ -21,7 +21,7 @@ public class ModBlocks {
             DeferredRegister.create(ForgeRegistries.BLOCKS, TCoreCreateMod.MOD_ID);
 
     public static final RegistryObject<Block> COCAINE_BLOCK = registerBlock("cocaine_block",
-            () -> new Block(BlockBehaviour.Properties.copy(Blocks.SAND).sound(SoundType.ROOTED_DIRT)));
+            () -> new FallingBlock(BlockBehaviour.Properties.copy(Blocks.SAND).sound(SoundType.ROOTED_DIRT)));
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
